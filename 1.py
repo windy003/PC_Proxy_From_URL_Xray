d61b2c9bae2525c9624c3a9c4a49731f2d0cb028
windy003 <928920471@qq.com>
2025-05-30 10:08:46 +0800

    在1.py顶部添加了pyinstaller的编译代码

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+#  pyinstaller --noconfirm --onefile --windowed --icon=icon.ico --add-data "icon.ico;."   --add-data "icon.png;." --add-data "xray.exe;."   1.py   --name  "proxyByUrl"
+
 # self.app_config_file = os.path.join(self.app_data_dir, 'app_config.json')
 
 
